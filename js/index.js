1290de5cc273e76ed4c079a93bdbbec24800d85b
cmcnvtuan11 <cmc.nvtuan11@partner.samsung.com>
2022-10-17 17:33:42 +0700

    update

--- js/index.js
+++ js/index.js
@@ -14,6 +14,9 @@ $(document).ready(function () {
     setting();
     viewListLevel();
     viewListLesson($("#level_select").val());
+
+    $('#wb_lesson_12').attr('checked', 'checked');
+    // start();
 });
 
 function viewListLevel() {
@@ -39,46 +42,95 @@ function viewListLesson(level) {
         indexWb++;
         if (level == x.Level) {
             $("#wordbook_lesson_div").append(
-                '<div>'
-                + '<input class="cursor_pointer" type="checkbox" value="' + x.Lesson + '" id="wb_lesson_' + indexWb + '">'
-                + '<label class="cursor_pointer" for="wb_lesson_' + indexWb + '">&nbsp;' + x.Lesson + '</label>'
-                + '</div>');
+                `<div>
+                    <input class="cursor_pointer" type="checkbox" value="${x.Lesson}" id="wb_lesson_${indexWb}">
+                    <label class="cursor_pointer" for="wb_lesson_'${indexWb}">&nbsp;${x.Lesson}</label>
+                </div>`);
         }
     });
 
-    // indexWb = 0;
     kanjiJson.forEach(x => {
         indexWb++;
         if (level == x.Level) {
             $("#kanji_lesson_div").append(
-                '<div>'
-                + '<input class="cursor_pointer" type="checkbox" value="' + x.Lesson + '" id="wb_lesson_' + indexWb + '">'
-                + '<label class="cursor_pointer" for="wb_lesson_' + indexWb + '">&nbsp;' + x.Lesson + '</label>'
-                + '</div>');
+                `<div>
+                    <input class="cursor_pointer" type="checkbox" value="${x.Lesson}" id="wb_lesson_${indexWb}">
+                    <label class="cursor_pointer" for="wb_lesson_${indexWb}">&nbsp;${x.Lesson}</label>
+                </div>`);
         }
     });
 
 }
 
 function start() {
-    // let listLesson = $("input[type=checkbox]:checked");
-    // let level = $("#level_select").val();
-    // let listWordbook = [];
-    // if (!listLesson || listLesson.length == 0) {
-    //     alert("Hãy chọn ít nhất 1 bài học");
-    //     return;
-    // }
-    // listLesson.each(x => {
-    //     lesson = listLesson[x];
-    //     tuVungJson.filter(y => y.Level == level && y.Lesson == lesson.value).forEach(z => {
-    //         listWordbook = listWordbook.concat(z.Data);
-    //     });
-    // });
+    let listLesson = $("input[type=checkbox]:checked");
+    let level = $("#level_select").val();
 
-    // listWordbook = derangeArray(listWordbook);
+    $("html").scrollTop(0);
+
+    let listWordbook = [];
+    if (!listLesson || listLesson.length == 0) {
+        alert("Hãy chọn ít nhất 1 bài học");
+        return;
+    }
+    listLesson.each(x => {
+        lesson = listLesson[x];
+        tuVungJson.filter(y => y.Level == level && y.Lesson == lesson.value).forEach(z => {
+            listWordbook = listWordbook.concat(z.Data);
+        });
+    });
+
+    listWordbook = derangeArray(listWordbook);
+    viewListWordbook(listWordbook);
 
     $("#menu_div").hide();
     $("#test_div").show();
+}
+
+function viewListWordbook(listWb, type) {
+    var html = "";
+    if (type == null) {
+        var index = 0;
+        listWb.forEach(x => {
+            var isMod = getRandomInt(0, 100) % 2 == 0;
+            var hiraShow = 'hide';
+            var meanShow = '';
+            if (isMod) {
+                var hiraShow = '';
+                var meanShow = 'hide';
+            }
+
+            html = html +
+                `<tr id="word_${index}">
+                    <td>
+                        <ruby class="${meanShow}">${x.Hira}
+                            <rt>${x.Kanji}</rt>
+                        </ruby>
+                        <input class="w-100 ${hiraShow}">
+                    </td>
+                    <td>
+                        <input class="${meanShow}">
+                        <span class="${hiraShow}">${x.Mean}</span>
+                    </td>
+                    <td class="td_btn_repeat hide">
+                        <input class="cursor_pointer" type="checkbox" id="word_repeat_${index}">
+                        <label class="cursor_pointer" for="word_repeat_${index}"><i class="fa-solid fa-repeat"></i></label>
+                    </td>
+                </tr>`;
+
+            index++;
+        })
+    }
+
+    $("#tbl_body_list_wordbook").html(html);
+}
+
+// help function 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function setting() {
 }
 
 function derangeArray(listItem) {
@@ -93,16 +145,4 @@ function derangeArray(listItem) {
 
     console.log(result);
     return result;
-}
-
-
-
-
-
-// help function 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function setting() {
 }
